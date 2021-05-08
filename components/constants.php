930743701bb1b9afa9a6e6fe62e8c0c0fa04742e
Virgile Mendes <mendes.virgile@gmail.com>
2021-05-08 18:18:04 +0200

    Update constants ROOT

--- components/constants.php
+++ components/constants.php
@@ -1,17 +1,17 @@
 <?php
 
 // La constante contenant le chemin vers la racine Serveur du projet
-// define('ROOT',$_SERVER['DOCUMENT_ROOT']);
-define('ROOT', $_SERVER['DOCUMENT_ROOT'] . '/virgile-men');
+define('ROOT',$_SERVER['DOCUMENT_ROOT']);
+// define('ROOT', $_SERVER['DOCUMENT_ROOT'] . '/virgile-men');
 
 // La constante contenant le chemin vers la racine Web du projet
-// define('WEBROOT','https://virgile.men');
-define('WEBROOT','http://localhost/virgile-men');
+define('WEBROOT','https://virgile.men');
+// define('WEBROOT','http://localhost/virgile-men');
 
 
 // La constante contenant le chemin vers la racine CDN du projet
-//define('CDNROOT','https://cdn.jsdelivr.net/gh/virgile-men/portfolio');
-define('CDNROOT','http://localhost/virgile-men');
+define('CDNROOT','https://cdn.jsdelivr.net/gh/virgile-men/portfolio');
+// define('CDNROOT','http://localhost/virgile-men');
 
 
 ?>
