4e15f41d598c4e0b7522df6843a9a8474ec413ce
Virgile Mendes <contact@virgile.men>
2026-01-04 13:22:19 +0100

    Refactor index.php to use relative paths for component includes, update educational and experience sections, and enhance project listings with new descriptions and formatting. Comment out unused video and project links for future reference.

--- index.php
+++ index.php
@@ -6,12 +6,12 @@
 
     include './components/constants.php';
 
-    include ROOT . '/components/head.php';
+    include './components/head.php';
 
 ?>
 <body>
     
-    <?php include ROOT . '/components/header.php'; ?>
+    <?php include './components/header.php'; ?>
 
     <div class="display-desktop-portfolio">
 
@@ -31,6 +31,13 @@
                         type="video/webm">
                 Désolé, votre navigateur ne prend pas en charge les vidéos intégrées.
             </video>
+            <!-- <video preload="none" id="moodle-alternatif" loop muted>
+                <source src="<?= CDNROOT ?>/assets/videos/moodle-alternatif-intro.mp4"
+                        type="video/mp4">
+                <source src="<?= CDNROOT ?>/assets/videos/moodle-alternatif-intro.webm"
+                        type="video/webm">
+                Désolé, votre navigateur ne prend pas en charge les vidéos intégrées.
+            </video> -->
             <video preload="none" id="netflix-ux" loop muted>
                 <source src="<?= CDNROOT ?>/assets/videos/netflixux-intro.mp4"
                         type="video/mp4">
@@ -62,13 +69,14 @@
             
         </div>
 
-        <?php include ROOT . '/components/alert.php'; ?> 
+	  <?php // include ROOT . '/components/alert.php'; ?> 
 
         <section class="projets">
             <h2>Sélection de projets</h2>
             <ul>
                 <li><a href="<?= WEBROOT ?>/projet/portrait-chinois/" data-project="portrait-chinois">Portrait-chinois</a></li>
                 <li><a href="<?= WEBROOT ?>/projet/flowwi/" data-project="flowwi">Flowwi</a></li>
+                <!-- <li><a href="<?= WEBROOT ?>/projet/moodle-alternatif/" data-project="moodle-alternatif">Moodle alternatif</a></li> -->
                 <li><a href="<?= WEBROOT ?>/projet/netflix-ux/" data-project="netflix-ux">Netflix UX</a></li>
                 <li><a href="<?= WEBROOT ?>/projet/finance-pour-tous/" data-project="finance-pour-tous">Finance pour tous</a></li>
                 <li><a href="<?= WEBROOT ?>/projet/nike-paris-on-air/" data-project="nike-paris-on-air">Nike Paris On Air</a></li>
@@ -92,6 +100,12 @@
                     <h3 class="title-project">Flowwi</h3>
                 </a>
             </div>
+            <!-- <div class="swiper-slide">
+                <a href="<?= WEBROOT ?>/projet/moodle-alternatif/">
+                    <img alt="Image de présentation de Moodle alternatif" src="<?= CDNROOT ?>/assets/videos/moodle-alternatif-intro.webp">
+                    <h3 class="title-project filter-invert">Moodle alternatif</h3>
+                </a>
+            </div> -->
             <div class="swiper-slide">
                 <a href="<?= WEBROOT ?>/projet/netflix-ux/">
                     <img alt="Image de présentation de Netflix UX" src="<?= CDNROOT ?>/assets/videos/netflixux-intro.webp">
@@ -122,13 +136,19 @@
     <section class="etudes">
         <h2>Études</h2>
         <article>
-            <h3>DUT Métiers du Multimédia et de l'Internet</h3>
+            <h3>Baccalauréat Génie logiciel</h3>
+            <h4>École de technologie supérieure</h4>
+            <h4>Sept. 2021 - Aujourd'hui</h4>
+            <p>Grâce à un programme universitaire et des expériences en milieu professionnel, je me forme aux postes de concepteur logiciel et responsable de développement.</p>
+        </article>
+        <article>
+            <h3>DUT Métiers du Multimédia et de l'Internet (équivalent DEC)</h3>
             <h4>Université Gustave Eiffel</h4>
-            <h4>Sept. 2019 - Aujourd'hui</h4>
+            <h4>Sept. 2019 - Juin 2021</h4>
             <p>Cursus sur les métiers du web, du développement back-end, à l’UX Design, en passant par le webmarketing ou la réalisation de produits audiovisuels.</p>
         </article>
         <article>
-            <h3>Baccalauréat Économique et Social</h3>
+            <h3>Baccalauréat Économique et Social (équivalent DES)</h3>
             <h4>Lycée La Salle</h4>
             <h4>Sept. 2017 - Juin 2019</h4>
             <p>Les principales matières sont les sciences sociales, l'économie et les mathématiques. Lorsque j'ai passé l'examen, j'ai suivi l'option "Histoire de l'art".</p>
@@ -140,24 +160,24 @@
 
 
     <section class="experiences">
-        <h2>Expérience</h2>
+        <h2>Expériences</h2>
         <article>
-            <h3>Développeur full-stack</h3>
+            <h3>Développeur Fullstack</h3>
+            <h4>Solution ISIOS</h4>
+            <h4>Janvier 2023 - Aujourd'hui</h4>
+            <p>Conçue pour répondre aux besoins des gestionnaires de cabinets d’ingénieurs, d’architectes, d’entreprises privées ou publiques, Solution ISIOS offre des services en gestion de projets, dont une application SaaS supportée par une équipe de développement logiciel.</p>
+        </article>
+        <article>
+            <h3>Programmeur .NET</h3>
+            <h4>Desjardins</h4>
+            <h4>Avril 2022 - Décembre 2022</h4>
+            <p>Au sein de la coopérative financière Desjardins, je participe à la maintenance et l'amélioration d'une application de traitements mathématiques pour calculer la distribution de coupon et de paiement des paniers de fonds de placement.</p>
+        </article>
+        <article>
+            <h3>Développeur Prestashop</h3>
             <h4>E-Smoke</h4>
-            <h4>Avril 2021 - Aujourd'hui</h4>
-            <p>Spécialisé dans le sevrage tabagique grâce au vapotage, E-Smoke compte 10 boutiques. En quête du marché internet, je développe le site e-commerce de l'enseigne.</p>
-        </article>
-        <article>
-            <h3>Chef de projet<br>Développeur back-end</h3>
-            <h4>Flowwi</h4>
-            <h4>Fév. - Juin 2020</h4>
-            <p>Flowwi est une plateforme web référençant près de 900 formations en France allant de Bac +3 à Bac +5 en lien direct avec le DUT Métiers du Multimédia et de l'Internet.</p>
-        </article>
-        <article>
-            <h3>Designer produit</h3>
-            <h4>Nike On Air</h4>
-            <h4>Fév. 2018</h4>
-            <p>Pour l'Air Max Day 2018, Nike a organisé l'événement Paris On Air. La marque a supervisé des ateliers pour la conception de la Air Max "Grand Paris".</p>
+            <h4>Avril 2021 - Juillet 2021</h4>
+            <p>Spécialisé dans le sevrage tabagique grâce au vapotage, E-Smoke compte 10 boutiques. En quête du marché internet, j'ai développé un site de commerce en ligne sous Prestashop (avec des règles CSS et animations JavaScript personnalisées).</p>
         </article>
     </section>
 
@@ -169,42 +189,66 @@
         <article>
             <h3>Développement Front-end</h3>
             <h4>HTML / CSS / Javascript ES6</h4>
-            <h4>jQuery / Swiper</h4>
-            <h4>React / React Native</h4>
-            <h4>Three.js</h4>
+            <h4>jQuery / Bootstrap</h4>
+            <h4>TypeScript / React</h4>
         </article>
+
+
         <article>
             <h3>Développement Back-end</h3>
-            <h4>PHP 7 / Symfony</h4>
+            <h4>PHP / Modèle MVC</h4>
             <h4>MySQL / NoSQL</h4>
             <h4>NodeJS / ExpressJS</h4>
-            <h4>PhpMyAdmin</h4>
+            <h4>PhpMyAdmin / MongoDB</h4>
         </article>
+
+
+        <article>
+            <h3>Développement logiciel</h3>
+            <h4>Java / Maven</h4>
+            <h4>C# .NET / Http Agility Pack / EPPlus</h4>
+            <h4>Playwright</h4>
+            <h4>Tests unitaire, d'acceptation et système</h4>
+        </article>
+
+
+        <article>
+            <h3>Gestion de projets</h3>
+            <h4>Méthode Agile</h4>
+            <h4>Confluence / Trello / Teams</h4>
+            <h4>Diagramme de Gantt</h4>
+        </article>
+
         <article>
             <h3>Design</h3>
+            <h4>Figma / Adobe XD</h4>
             <h4>Photoshop / Illustrator</h4>
-            <h4>Adobe XD</h4>
             <h4>After Effect / Premiere Pro</h4>
         </article>
-        <article>
-            <h3>Outils</h3>
-            <h4>Github / NPM</h4>
-            <h4>Google Workspace</h4>
-            <h4>Wordpress</h4>
-            <h4>Prestashop / Woocommerce</h4>
-        </article>
-        <article>
-            <h3>Méthodologies</h3>
-            <h4>Méthode Agile</h4>
-            <h4>Programmation orienté objet</h4>
-            <h4>Conception de base de données</h4>
-        </article>
+
+
         <article>
             <h3>Langues</h3>
-            <h4>Français - Maternelle</h4>
-            <h4>Anglais - Intermédiaire B1</h4>
-            <h4>Portugais - Oral maternelle</h4>
+            <h4>Français - Natale</h4>
+            <h4>Anglais - Intermédiaire</h4>
+            <h4>Portugais - Oral natale</h4>
         </article>
+
+        <article>
+            <h3>Outils</h3>
+            <h4>Git / Github / Gitlab</h4>
+            <h4>Google Workspace / Microsoft Office</h4>
+            <h4>Wordpress / Divi / Elementor</h4>
+            <h4>Prestashop / Woocommerce</h4>
+        </article>
+
+        <article>
+            <h3>Conventions & Architectures</h3>
+            <h4>Conception de base de données</h4>
+            <h4>Patrons de conception GoF</h4>
+            <h4>Principes GRASP</h4>
+        </article>
+
     </section>
 
 
@@ -215,12 +259,12 @@
             <h4>Louvre, Galerie Itinerrance, Fondation Louis Vuitton</h4>
         </article>
         <article>
-            <h3>Rap français</h3>
-            <h4>Hamza, Kekra, Dinos</h4>
+            <h3>Passions suivi hebdomadairement</h3>
+            <h4>Formule 1, Star Wars, Rap français</h4>
         </article>
         <article>
             <h3>Films d'animation</h3>
-            <h4>Spider-Man: New Generation, La Passion Van Gogh</h4>
+            <h4>Spider-Man: New Generation, La Passion Van Gogh, Le Chat Potté 2</h4>
         </article>
     </section>
 
